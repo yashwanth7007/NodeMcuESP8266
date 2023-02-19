d71c43f40bda584d4884aef36a8eca05d0176c04
Yashwanth_Kumar_M_S <Yashwanth.KumarMS@analog.com>
2023-02-19 22:02:35 +0530

    TCP connection for I2C added but not tested.

--- I2C/I2C/src/main.cpp
+++ I2C/I2C/src/main.cpp
@@ -1,6 +1,17 @@
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <stdint.h>
+#include "msg.h"
+
+
+uint8_t MSGBuffer[sizeof(Msg)];
+
+int port = 8888;  //Port number
+WiFiServer server(port);
+
+const char* ssid = "NimdNeevBaluskoli";
+const char* password = "81194263441";
+
 
 typedef enum {
   I2C_STATUS_SUCCESS,
@@ -36,11 +47,86 @@ I2C_STATUS i2cWriteRead(uint16_t i2cAddress, uint16_t nWrLen, uint8_t* wrBuff, u
 void setup() {
  Serial.begin(9600); /* begin serial for debug */
  Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
+
+ WiFi.mode(WIFI_STA);
+ WiFi.begin(ssid, password);
+ Serial.println("Connecting to Wifi");
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
+   Serial.print(".");
+   delay(500);
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP()); 
+
+
+  server.begin();
+
+  Serial.print(WiFi.localIP());
+  Serial.print(" on port ");
+  Serial.println(port);
+
+
 }
 
 void loop() {
 
-  uint8_t wrBuff = 0x02;
+
+
+  WiFiClient client = server.available();
+  
+  if (client) {
+    if(client.connected())
+    {
+      Serial.println("Client Connected");
+    }
+    
+    while(client.connected()){  
+      int index = 0;    
+      while(client.available()>0){
+        // read data from the connected client
+
+        index += client.readBytes(&MSGBuffer[index], client.available()); 
+      }
+
+      if (index != sizeof(Msg))
+      {
+        Serial.println("MSG not completly recieved");
+      }
+
+      //Verify the header
+
+      Msg* msg = (Msg*)&MSGBuffer[0];
+
+
+
+      if (msg->hdr.signature != 0x1234)
+      {
+        Serial.println("No proper connection");
+      }
+      //Send Data to connected client
+      while(Serial.available()>0)
+      {
+        client.write(Serial.read());
+      }
+
+      //client.write("Hello From Server");
+    }
+    client.stop();
+    Serial.println("Client disconnected"); 
+  }
+
+
+
+
+
+
+/*  uint8_t wrBuff = 0x02;
   uint8_t rdBuff[4] = {0,0,0,0};
   i2cWriteRead(0x6A, 1, &wrBuff, 3, &rdBuff[0]);
 
@@ -48,6 +134,7 @@ void loop() {
   {
     Serial.printf("rdBuff[%d] = %x\n", i, rdBuff[i]);
   }
+  */
 
  Serial.println();
  delay(1000);
